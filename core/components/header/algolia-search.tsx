96681ce0f8346e40e6fca64c69362b8d3a17aa8c
Justin German <justin.german23@gmail.com>
2025-03-27 12:18:28 -0500

    wip global search styles

--- core/components/header/algolia-search.tsx
+++ core/components/header/algolia-search.tsx
@@ -2,8 +2,12 @@
 
 import { Hit } from 'algoliasearch';
 import { liteClient as algoliasearch } from 'algoliasearch/lite';
+import { clsx } from 'clsx';
+import { Search, X } from 'lucide-react';
 import { Hits, InstantSearch, SearchBox, useHits, useSearchBox } from 'react-instantsearch';
 
+import { Button } from '@/vibes/soul/primitives/button';
+import { Image } from '~/components/image';
 import { Link } from '~/components/link';
 
 const searchClient = algoliasearch(
@@ -13,6 +17,7 @@ const searchClient = algoliasearch(
 
 interface HitFields {
   fields: {
+    pageImage: { 'en-US': string } | null;
     pageName: { 'en-US': string } | null;
     productName: { 'en-US': string } | null;
     pageSlug: { 'en-US': string } | null;
@@ -24,6 +29,7 @@ interface HitFields {
 function Result({ hit }: { hit: Hit<HitFields> }) {
   const fields = hit.fields;
 
+  const pageImage = fields.pageImage ? fields.pageImage['en-US'] : null;
   const pageName = fields.pageName ? fields.pageName['en-US'] : null;
   const productName = fields.productName ? fields.productName['en-US'] : null;
   const pageSlug = fields.pageSlug ? fields.pageSlug['en-US'] : null;
@@ -31,43 +37,80 @@ function Result({ hit }: { hit: Hit<HitFields> }) {
   const shortDescription = fields.shortDescription ? fields.shortDescription['en-US'] : null;
 
   return (
-    <div className="mb-6 flex flex-col gap-2">
-      {pageName ? <p className="text-sm font-bold">Page Name: {pageName}</p> : null}
-      {productName ? <p className="text-sm font-bold">Product Name: {productName}</p> : null}
-      {pageSlug ? (
-        <p className="text-sm">
-          Page Slug:{' '}
-          <Link className="text-blue-500" href={`/${pageSlug}`} prefetch="hover">
-            {pageSlug}
-          </Link>
-        </p>
-      ) : null}
-      {pageDescription ? <p className="text-sm">Page Description: {pageDescription}</p> : null}
-      {shortDescription ? <p className="text-sm">Short Description: {shortDescription}</p> : null}
-    </div>
+    <article className="group relative">
+      {pageImage ? (
+        <figure className="aspect-square h-auto w-full rounded-lg bg-surface-image">
+          <Image
+            alt={pageName ? `Image for ${pageName}` : 'Search result image'}
+            className="object-cover"
+            src={pageImage}
+          />
+        </figure>
+      ) : (
+        <figure className="aspect-square h-auto w-full rounded-lg bg-surface-image" />
+      )}
+      <div className="py-2">
+        {pageName ? <p className="text-xl font-medium">{pageName}</p> : null}
+        {productName ? <p className="text-sm font-bold">{productName}</p> : null}
+        {pageSlug ? (
+          <p className="text-sm">
+            <Link
+              className="transition-colors duration-200 ease-quad after:absolute after:inset-0 group-hover:text-primary"
+              href={`/${pageSlug}`}
+              prefetch="hover"
+            >
+              {pageSlug}
+            </Link>
+          </p>
+        ) : null}
+        {pageDescription ? <p className="text-sm">Page Description: {pageDescription}</p> : null}
+        {shortDescription ? <p className="text-sm">Short Description: {shortDescription}</p> : null}
+      </div>
+    </article>
   );
 }
 
-function SearchComponent() {
+interface SearchComponentProps {
+  closeSearch?: () => void;
+}
+
+function SearchComponent({ closeSearch }: SearchComponentProps) {
   const { query } = useSearchBox();
   const { items } = useHits();
 
+  const formStyles =
+    '[&_form]:flex [&_form]:gap-4 [&_form_button.ais-SearchBox-submit]:hidden [&_form_button.ais-SearchBox-reset]:hidden';
+  const inputStyles =
+    '[&_input]:flex-1 [&_input]:min-h-12 [&_input]:focus-within:outline-0 [&_input::-webkit-search-cancel-button]:appearance-none';
+  const searchHitsStyles = 'grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4';
+
   return (
     <>
-      <SearchBox />
-      {query && items.length > 0 ? <Hits hitComponent={Result} /> : null}
+      <div className="flex items-center gap-2 border-b border-border transition-colors duration-200 ease-quad focus-within:border-primary">
+        <Button onClick={closeSearch} shape="link" size="link" variant="link">
+          <X size={24} strokeWidth={1} />
+        </Button>
+        <Search size={24} strokeWidth={1} />
+        <SearchBox
+          className={clsx('flex-1', formStyles, inputStyles)}
+          placeholder="Search Lotus for products, guides, or resources…"
+        />
+      </div>
+      {query && items.length > 0 ? (
+        <Hits classNames={{ root: 'mt-16', list: clsx(searchHitsStyles) }} hitComponent={Result} />
+      ) : null}
     </>
   );
 }
 
-export default function AlgoliaSearch() {
+export default function AlgoliaSearch({ closeSearch }: SearchComponentProps) {
   return (
-    <div className="flex items-center gap-3 px-3 py-3 @4xl:px-5 @4xl:py-4">
+    <div className="container overflow-y-auto py-8">
       <InstantSearch
         indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME}
         searchClient={searchClient}
       >
-        <SearchComponent />
+        <SearchComponent closeSearch={closeSearch} />
       </InstantSearch>
     </div>
   );
