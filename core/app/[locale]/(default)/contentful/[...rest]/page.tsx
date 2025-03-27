ee120b4c21aa03b1cbdf486483ab89dbe4e51943
Keith Knoer <kknoer@gmail.com>
2025-03-27 10:39:11 -0400

    refactor product page to separate Contentful data fetching and update type definitions for improved clarity

--- core/app/[locale]/(default)/contentful/[...rest]/page.tsx
+++ core/app/[locale]/(default)/contentful/[...rest]/page.tsx
@@ -36,14 +36,14 @@ export default async function ContentfulPage({ params }: Props) {
       {pageDescription ? <div dangerouslySetInnerHTML={{ __html: pageDescription }} /> : null}
       {Array.isArray(pageContent) &&
         pageContent.map((field: PageContentField) => (
-          <>
+          <div key={field.sys.id}>
             {field.sys.contentType.sys.id === 'button' ? (
               <div key={field.sys.id}>Button Display Component</div>
             ) : null}
             {field.sys.contentType.sys.id === 'faq' ? (
               <div key={field.sys.id}>FAQ Display Component</div>
             ) : null}
-          </>
+          </div>
         ))}
     </div>
   );
