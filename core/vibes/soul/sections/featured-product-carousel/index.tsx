25db6b8c44cc6dea9ce1b6e31535d13233c1a039
Keith Knoer <kknoer@gmail.com>
2025-04-21 13:51:38 -0400

    lint fix

--- core/vibes/soul/sections/featured-product-carousel/index.tsx
+++ core/vibes/soul/sections/featured-product-carousel/index.tsx
@@ -50,7 +50,7 @@ export function FeaturedProductCarousel({
   hideOverflow = false,
 }: FeaturedProductCarouselProps) {
   return (
-    <SectionLayout containerSize="2xl" hideOverflow={true}>
+    <SectionLayout containerSize="2xl">
       <div className="mb-6 flex w-full flex-row flex-wrap items-end justify-between gap-x-8 gap-y-6 @4xl:mb-8">
         <header className="font-[family-name:var(--featured-product-carousel-font-family,var(--font-family-body))]">
           <h2 className="font-[family-name:var(--featured-product-carousel-title-font-family,var(--font-family-heading))] text-2xl leading-none text-[var(--featured-product-carousel-title,hsl(var(--foreground)))] @xl:text-3xl @4xl:text-4xl">
