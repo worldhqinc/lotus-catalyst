ee51f242462b826d73e96bc3943fa0bacfda1e25
Justin German <justin.german23@gmail.com>
2025-04-30 11:16:30 -0500

    updates Reset password styles

--- core/vibes/soul/sections/forgot-password-section/forgot-password-form.tsx
+++ core/vibes/soul/sections/forgot-password-section/forgot-password-form.tsx
@@ -41,7 +41,7 @@ export function ForgotPasswordForm({
   });
 
   return (
-    <form {...getFormProps(form)} action={formAction} className="flex grow flex-col gap-5">
+    <form {...getFormProps(form)} action={formAction} className="flex grow flex-col gap-8">
       <Input
         {...getInputProps(fields.email, { type: 'text' })}
         errors={fields.email.errors}
@@ -65,7 +65,13 @@ function SubmitButton({ children }: { children: React.ReactNode }) {
   const { pending } = useFormStatus();
 
   return (
-    <Button className="mt-auto w-full" loading={pending} type="submit" variant="secondary">
+    <Button
+      className="mt-auto w-full @4xl:max-w-max"
+      loading={pending}
+      size="medium"
+      type="submit"
+      variant="primary"
+    >
       {children}
     </Button>
   );
