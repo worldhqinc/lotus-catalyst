2b2d079d737b120848b9bc0e53b075d765470f73
Justin German <justin.german23@gmail.com>
2025-05-08 17:07:01 -0500

    redirects policies parent to privacy policy

--- core/middlewares/with-routes.ts
+++ core/middlewares/with-routes.ts
@@ -271,6 +271,11 @@ export const withRoutes: MiddlewareFactory = () => {
   return async (request, event) => {
     const locale = request.headers.get('x-bc-locale') ?? '';
 
+    // Redirects /policies to /policies/privacy-policy
+    if (request.nextUrl.pathname === '/policies/') {
+      return NextResponse.redirect(new URL('/policies/privacy-policy', request.url), { status: 301 });
+    }
+
     const { route, status } = await getRouteInfo(request, event);
 
     if (status === 'MAINTENANCE') {
