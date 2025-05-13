7fe85ed0b244aac55b695f371f5938e51374714e
Keith Knoer <kknoer@gmail.com>
2025-05-13 16:29:00 -0400

    contentful/algolia product webhook

--- core/app/api/contentful/webhook/[id]/route.ts
+++ core/app/api/contentful/webhook/[id]/route.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 
 import { productFinishedGoodsSchema } from '~/contentful/schema';
 import { contentfulClient } from '~/lib/contentful';
+import { ensureImageUrl } from '~/lib/utils';
 
 export async function POST(request: NextRequest, context: { params: Promise<{ id: string }> }) {
   const { id } = await context.params;
@@ -20,6 +21,8 @@ export async function POST(request: NextRequest, context: { params: Promise<{ id
     const entry = await contentfulClient.getEntry(id, {
       include: 10,
     });
+
+    const parsedEntry = productFinishedGoodsSchema.parse(entry);
 
     const algoliaClient = algoliasearch(
       process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? '',
