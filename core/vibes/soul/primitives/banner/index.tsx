1e7515c61d6fa44fc19c8840353de83e10cbb9d6
Justin German <justin.german23@gmail.com>
2025-05-16 12:31:18 -0500

    updates accessibility icon

--- core/vibes/soul/primitives/banner/index.tsx
+++ core/vibes/soul/primitives/banner/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { clsx } from 'clsx';
-import { PersonStandingIcon } from 'lucide-react';
 import { ForwardedRef, forwardRef, ReactNode } from 'react';
 
 import { Stream, Streamable } from '@/vibes/soul/lib/streamable';
@@ -77,9 +76,50 @@ export const Banner = forwardRef(
               </li>
               <li>
                 <div className="flex items-center gap-2">
-                  <div className="rounded-full border-2 border-white">
-                    <PersonStandingIcon size={22} strokeWidth={2} />
-                  </div>
+                  <svg
+                    fill="none"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                    />
+                    <path
+                      d="M12 7C12.5523 7 13 6.55228 13 6C13 5.44772 12.5523 5 12 5C11.4477 5 11 5.44772 11 6C11 6.55228 11.4477 7 12 7Z"
+                      fill="#131313"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                    />
+                    <path
+                      d="M9 18L12 12L15 18"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                    />
+                    <path
+                      d="M7 8L12 10L17 8"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                    />
+                    <path
+                      d="M12 10V12"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                    />
+                  </svg>
                   <p className="hidden @4xl:block">Accessibility</p>
                 </div>
               </li>
