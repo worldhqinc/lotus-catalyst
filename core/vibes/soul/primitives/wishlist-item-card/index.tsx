b40477b16616f1ae7a89d69c2351ecf5b88724c4
Justin German <justin.german23@gmail.com>
2025-05-19 11:13:50 -0500

    updates wishlist grid

--- core/vibes/soul/primitives/wishlist-item-card/index.tsx
+++ core/vibes/soul/primitives/wishlist-item-card/index.tsx
@@ -41,7 +41,7 @@ export const WishlistItemCard = ({
       className="relative flex max-w-md basis-[calc(100%-1rem)] flex-col justify-between gap-3 @md:basis-[calc(50%-0.75rem)] @lg:basis-[calc(33%-0.5rem)] @2xl:basis-[calc(25%-0.25rem)]"
       key={product.id}
     >
-      <ProductCard aspectRatio="3:4" product={product} showCompare={false} {...props} />
+      <ProductCard aspectRatio="1:1" product={product} showCompare={false} {...props} />
       {removeAction && (
         <div className="absolute -top-3 -right-3 rounded-full transition-shadow duration-100 hover:shadow-md">
           <RemoveWishlistItemButton action={removeAction} itemId={itemId} wishlistId={wishlistId} />
@@ -59,7 +59,7 @@ export function WishlistItemSkeleton({ className = '' }: { className?: string })
         className,
       )}
     >
-      <ProductCardSkeleton aspectRatio="3:4" />
+      <ProductCardSkeleton aspectRatio="1:1" />
       <Skeleton.Box className="min-h-10 rounded-full" />
     </div>
   );
