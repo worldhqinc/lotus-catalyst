afc0f7805cb1d142418b8039ac5a59e9f47fccac
Keith Knoer <kknoer@gmail.com>
2025-05-19 20:36:39 -0400

    refactor support links in FeatureItem component to include published version check and improve URL handling

--- core/components/contentful/sections/feature-item.tsx
+++ core/components/contentful/sections/feature-item.tsx
@@ -18,19 +18,27 @@ export function FeatureItem({
   reverseOrder = false,
 }: FeatureItemProps) {
   const supportLinks =
-    products?.map((product) => {
-      const docs = product.fields.docs?.find(
-        (doc): doc is supportDocument => doc.fields.documentType === 'Use and Care Guide',
-      );
+    products
+      ?.map((product) => {
+        const docs = product.fields.docs?.find((doc): doc is supportDocument => {
+          if (!doc.sys.publishedVersion) return false;
 
-      return {
-        label: product.fields.webProductName,
-        value: docs?.fields.url ?? '',
-      };
-    }) ?? [];
+          return doc.fields.documentType === 'Use and Care Guide';
+        });
+
+        if (!docs) return null;
+
+        return {
+          label: product.fields.webProductName,
+          value: `${product.fields.webProductName}|${docs.fields.url}`,
+        };
+      })
+      .filter((link) => link !== null) ?? [];
 
   const handleSupportProductSelect = (value: string) => {
-    window.open(value, '_blank');
+    const [, url] = value.split('|');
+
+    window.open(url, '_blank');
   };
 
   return (
@@ -38,12 +46,14 @@ export function FeatureItem({
       <div className="flex flex-col items-start justify-start lg:px-12">
         <h3 className="text-4xl">{heading}</h3>
         {description ? <p className="text-contrast-400 mt-2">{description}</p> : null}
-        <Select
-          className="mt-12"
-          name="support_product_select"
-          onValueChange={handleSupportProductSelect}
-          options={supportLinks}
-        />
+        {supportLinks.length > 0 ? (
+          <Select
+            className="mt-12"
+            name="support_product_select"
+            onValueChange={handleSupportProductSelect}
+            options={supportLinks}
+          />
+        ) : null}
       </div>
       <div
         className={`bg-surface-image order-first aspect-[4/3] w-full overflow-hidden rounded-lg ${
