af8737a3a758b56c1d77f518fc593665635dbbc1
Justin German <justin.german23@gmail.com>
2025-05-27 14:59:45 -0500

    removes headroom and updates navigation position property

--- core/vibes/soul/sections/header-section/index.tsx
+++ core/vibes/soul/sections/header-section/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ComponentPropsWithoutRef, useEffect, useRef, useState } from 'react';
-import Headroom from 'react-headroom';
 
 import { Banner } from '@/vibes/soul/primitives/banner';
 import { Navigation } from '@/vibes/soul/primitives/navigation';
@@ -12,26 +11,8 @@ export interface HeaderProps {
 }
 
 export function HeaderSection({ navigation, banner }: HeaderProps) {
-  const [bannerElement, setBannerElement] = useState<HTMLElement | null>(null);
-  const [bannerHeight, setBannerHeight] = useState(0);
   const [isFloating, setIsFloating] = useState(false);
   const headerRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (!bannerElement) return;
-
-    const resizeObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
-      entries.forEach((entry) => {
-        setBannerHeight(entry.contentRect.height);
-      });
-    });
-
-    resizeObserver.observe(bannerElement);
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [bannerElement]);
 
   useEffect(() => {
     function setHeaderHeightVar() {
@@ -49,23 +30,27 @@ export function HeaderSection({ navigation, banner }: HeaderProps) {
     return () => window.removeEventListener('resize', setHeaderHeightVar);
   }, []);
 
+  useEffect(() => {
+    function handleScroll() {
+      setIsFloating(window.scrollY > 36);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div>
-      <Banner ref={setBannerElement} {...banner}>
-        {banner?.children || null}
-      </Banner>
-      <div className="bg-[var(--header-background,hsl(var(--background)))]" ref={headerRef}>
-        <Headroom
-          className="[&_.headroom--unpinned]:![transform:translate3d(0,0,0)]"
-          onUnfix={() => setIsFloating(false)}
-          onUnpin={() => setIsFloating(true)}
-          pinStart={bannerHeight}
-        >
-          <div>
-            <Navigation {...navigation} isFloating={isFloating} />
-          </div>
-        </Headroom>
+    <>
+      <Banner {...banner}>{banner?.children || null}</Banner>
+      <div
+        className="sticky top-0 z-20 bg-[var(--header-background,hsl(var(--background)))]"
+        ref={headerRef}
+      >
+        <div>
+          <Navigation {...navigation} isFloating={isFloating} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
