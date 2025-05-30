c66644b69f14449d1a1b69da711df30080cd83fa
Justin German <justin.german23@gmail.com>
2025-05-30 15:43:19 -0500

    updates pdp padding block, update product gallery border raduis

--- core/vibes/soul/sections/product-detail/index.tsx
+++ core/vibes/soul/sections/product-detail/index.tsx
@@ -133,7 +133,7 @@ export function ProductDetail<F extends Field>({
                       }
                     />
                   </div>
-                  <div className="col-span-full px-4 py-8 @xl:px-0 @xl:py-0 @2xl:col-span-6 @5xl:col-span-5">
+                  <div className="col-span-full px-4 py-8 @xl:px-0 @2xl:col-span-6 @2xl:py-0 @5xl:col-span-5">
                     <div className="mb-8 flex items-center justify-between gap-4">
                       <div className="flex gap-2">
                         {contentful.fields.webProductLine?.map((line, index) => (
