eefe85667862bcb8cfeed011c737c9ddf4875e50
Jordan Arldt <jordan.arldt@bigcommerce.com>
2025-06-03 14:32:34 -0500

    Translate search button on NotFound page (#2366)

--- core/app/[locale]/not-found.tsx
+++ core/app/[locale]/not-found.tsx
@@ -27,6 +27,7 @@ export default async function NotFound({ searchParams }: { searchParams: SearchP
         <Header />
         <NotFoundSection
           className="flex-1 place-content-center"
+          ctaLabel={t('search')}
           subtitle={t('subtitle')}
           title={t('title')}
         />
