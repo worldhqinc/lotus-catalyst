2204d69f62343c2f84d8a6db26c62560b1c8a37c
Justin German <justin.german23@gmail.com>
2025-06-04 13:23:55 -0500

    updates slideshow heading tags

--- core/vibes/soul/sections/slideshow/index.tsx
+++ core/vibes/soul/sections/slideshow/index.tsx
@@ -197,9 +197,15 @@ export function Slideshow({
                       )}
                     >
                       {title ? (
-                        <h1 className="m-0 max-w-xl font-[family-name:var(--slideshow-title-font-family,var(--font-family-heading))] text-4xl leading-none font-medium text-[var(--slideshow-title,hsl(var(--background)))] @2xl:text-5xl @2xl:leading-[.9] @4xl:text-6xl">
-                          {title}
-                        </h1>
+                        idx === 0 ? (
+                          <h1 className="m-0 max-w-xl font-[family-name:var(--slideshow-title-font-family,var(--font-family-heading))] text-4xl leading-none font-medium text-[var(--slideshow-title,hsl(var(--background)))] @2xl:text-5xl @2xl:leading-[.9] @4xl:text-6xl">
+                            {title}
+                          </h1>
+                        ) : (
+                          <h2 className="m-0 max-w-xl font-[family-name:var(--slideshow-title-font-family,var(--font-family-heading))] text-4xl leading-none font-medium text-[var(--slideshow-title,hsl(var(--background)))] @2xl:text-5xl @2xl:leading-[.9] @4xl:text-6xl">
+                            {title}
+                          </h2>
+                        )
                       ) : null}
                       {showDescription && (
                         <p className="mt-2 max-w-xl font-[family-name:var(--slideshow-description-font-family,var(--font-family-body))] text-base leading-normal text-[var(--slideshow-description,hsl(var(--background)/80%))] @xl:mt-3 @xl:text-lg">
